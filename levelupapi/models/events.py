354ddd7f1bf77acc26a62f06eb039d2eeae24ce0
EugeneTerry <eugene.terry73@gmail.com>
2021-12-07 21:34:27 -0600

    migratated

--- levelupapi/models/events.py
+++ levelupapi/models/events.py
@@ -1,9 +1,9 @@
 from django.db import models
-from gamers import Gamer
+from .gamers import Gamer
 
 class Events(models.Model):
   
     title = models.CharField(max_length=50)
     date = models.DateTimeField()
     game = models.CharField(max_length=50)
-    creator = models.ManyToOneRel(Gamer, related_name = 'events_created')
+    # creator = models.ManyToOneRel(Gamer, related_name = 'events_created')
--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -1,8 +1,8 @@
 from django.db import models
-from gamers import Gamer
+from .gamers import Gamer
 
 class Games(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
-    game_creator = models.OneToOneField(Gamer, on_delete=models.CASCADE)
+    # game_creator = models.OneToOneField(Gamer, on_delete=models.CASCADE())
     
